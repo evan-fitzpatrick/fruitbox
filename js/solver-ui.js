03cb44427cce2a2de2a5d4c457f95c27b50924b0
evan-fitzpatrick <60202764+evan-fitzpatrick@users.noreply.github.com>
2025-04-02 19:05:26 -0700

    Update solver-ui.js

--- js/solver-ui.js
+++ js/solver-ui.js
@@ -52,7 +52,7 @@ class SolverUIController {
       { value: 'greedy', label: 'Greedy Search' },
       { value: 'invgreedy', label: 'Inverse Greedy' },
       { value: 'maxoptions', label: 'Max Moves' },
-      { value: 'minpotentialloss', label: 'Max Potential Moves' }, 
+      { value: 'minpotentialloss', label: 'Min Move Cost' }, 
       { value: 'frequency3', label: '3n Frequency' },
       { value: 'frequency4', label: '4n Frequency' },
       { value: 'frequency5', label: '5n Frequency' },
